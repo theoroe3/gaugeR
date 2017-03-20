5499eb79dad179fffa6d4a64b8a371ba66baf0f4
jamieRowen <jamie@jumpingrivers.com>
2017-03-20 16:52:28 +0000

    added source for gauge widget

---
+++ R/gauge.R
@@ -0,0 +1,51 @@
+#' <Add Title>
+#'
+#' <Add Description>
+#'
+#' @import htmlwidgets
+#'
+#' @export
+gauge <- function(value, width = NULL, height = NULL) {
+
+  # forward options using x
+  x = list(
+    data = value
+  )
+
+  # create widget
+  htmlwidgets::createWidget(
+    name = 'gauge',
+    x,
+    width = width,
+    height = height,
+    package = 'guageR'
+  )
+}
+
+#' Shiny bindings for gauge
+#'
+#' Output and render functions for using gauge within Shiny
+#' applications and interactive Rmd documents.
+#'
+#' @param outputId output variable to read from
+#' @param width,height Must be a valid CSS unit (like \code{'100\%'},
+#'   \code{'400px'}, \code{'auto'}) or a number, which will be coerced to a
+#'   string and have \code{'px'} appended.
+#' @param expr An expression that generates a gauge
+#' @param env The environment in which to evaluate \code{expr}.
+#' @param quoted Is \code{expr} a quoted expression (with \code{quote()})? This
+#'   is useful if you want to save an expression in a variable.
+#'
+#' @name gauge-shiny
+#'
+#' @export
+gaugeOutput <- function(outputId, width = '100%', height = '400px'){
+  htmlwidgets::shinyWidgetOutput(outputId, 'gauge', width, height, package = 'guageR')
+}
+
+#' @rdname gauge-shiny
+#' @export
+renderGauge <- function(expr, env = parent.frame(), quoted = FALSE) {
+  if (!quoted) { expr <- substitute(expr) } # force quoted
+  htmlwidgets::shinyRenderWidget(expr, gaugeOutput, env, quoted = TRUE)
+}
